9131319e42d02fbc8fa14a186191fe248dd5338b
Alekey Kiselev <kanic.alex@gmail.com>
2023-05-30 15:47:34 +0700

    added a checking 'metric' attribute

--- assets/js/script-js.js
+++ assets/js/script-js.js
@@ -17,18 +17,18 @@ function loadPage() {
     }
 }
 function applyEffect(event) {
-    var aggregatedId = event.target.id;
+    var element = event.target;
+    var aggregatedId = element.id;
     var subIds = aggregatedId.split('-');
-    event.className = 'tdMarked';
-    var isElementMetric = getCustomAttributeValue(event, 'metric') == 'true';
+    var isElementMetric = getCustomAttributeValue(element, 'metric') == 'true';
     var log = '';
-    for (var i = 0; i < event.target.parentElement.children.length; i++) {
-        var child = event.target.parentElement.children[i];
+    for (var i = 0; i < element.parentElement.children.length; i++) {
+        var child = element.parentElement.children[i];
         for(var j = 0; j < subIds.length; j++) {
             var id = subIds[j];
             log += child.id + '  vs  ' + id + '|' + child.id.includes(id) + '<br/>';
             var isChildElementMetric = getCustomAttributeValue(child, 'metric') == 'true';
-            if(event.type == 'mouseover' && (!isElementMetric || !isChildElementMetric) && id.length > 0 && child.id.includes(id)) {
+            if(event.type == 'mouseover' && (element == child || !isElementMetric || !isChildElementMetric) && id.length > 0 && child.id.includes(id)) {
                 child.className = 'tdMarked';
                 break;
             } else {
