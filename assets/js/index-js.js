2bd60116d4c6be552ab7a5b95891f53274510fde
Alekey Kiselev <kanic.alex@gmail.com>
2025-02-20 18:55:10 +0700

    updated the index page + added new reports

--- assets/js/index-js.js
+++ assets/js/index-js.js
@@ -3,7 +3,7 @@ var SCRIPT_RUNNER_PATH = "rest/scriptrunner/latest/custom";
 var DATE_FROM = "2023-07-01";
 var FOR_LAST_MONTHS = 3;
 
-function gotoCollectionMetrics(reportType) {
+function gotoCollectionMetricsBoard(reportType) {
 // window.open('https://jira.ftc.ru/rest/scriptrunner/latest/custom/getCollectionMetrics?dateFrom=2023-01-01&dateTo=2023-06-01&size=small&done&report=epicsTimeMetrics', '_blank'); return false;
 // window.open('https://jira.ftc.ru/rest/scriptrunner/latest/custom/getCollectionMetrics?report=sprintsInWork', '_blank'); return false;
     var url = JIRA_URL + "/" + SCRIPT_RUNNER_PATH + "/getCollectionMetrics?";
@@ -18,6 +18,65 @@ function gotoCollectionMetrics(reportType) {
             url += "user=" + "???" + "&";
     }
     url += "report=" + reportType;
+    window.open(url, "_blank");
+    return false;
+}
+
+function gotoDeptSLABoard_Mirutov(subdivision) {
+// window.open('https://jira.ftc.ru/rest/scriptrunner/latest/custom/getEpicsTimeMetrics2?dateFrom=2023-04-01&dateTo=2023-06-30&ra=cl&size=small&done', '_blank'); return false;
+    var url = JIRA_URL + "/" + SCRIPT_RUNNER_PATH + "/getDeptSLAReport?";
+
+    url += "dateFrom=" + dateToYYYYMMDD(prepareFromDate(new Date())) + "&";
+    url += "dateTo=" + dateToYYYYMMDD(new Date()) + "&";
+    url += "dept=" + subdivision + "&";
+    url += "showEpicName" + "&";
+    url += "printJQL";
+    window.open(url, "_blank");
+    return false;
+}
+
+function gotoFlowTimeMetricsBoard_Mirutov(subdivision) {
+// window.open('https://jira.ftc.ru/rest/scriptrunner/latest/custom/getEpicsTimeMetrics2?dateFrom=2023-04-01&dateTo=2023-06-30&ra=cl&size=small&done', '_blank'); return false;
+    var url = JIRA_URL + "/" + SCRIPT_RUNNER_PATH + "/getFlowTimeMetrics?";
+    url += "dateFrom=" + dateToYYYYMMDD(prepareFromDate(new Date())) + "&";
+    url += "dateTo=" + dateToYYYYMMDD(new Date()) + "&";
+    url += "depts=" + subdivision;
+    window.open(url, "_blank");
+    return false;
+}
+
+function gotoStoryPointsBoard_Mirutov(subdivision) {
+// window.open('https://jira.ftc.ru/rest/scriptrunner/latest/custom/getStoryPointsBoard?subdivision=%D0%9A%D0%BE%D0%BB%D0%BB%D0%B5%D0%BA%D1%82%D0%BE%D1%80', '_blank'); return false;
+    var url = JIRA_URL + "/" + SCRIPT_RUNNER_PATH + "/getStoryPointsBoard?";
+    url += "subdivision=" + subdivision;
+    window.open(url, "_blank");
+    return false;
+}
+
+function gotoReopenedBoard_Mirutov(subdivision) {
+    var url = JIRA_URL + "/" + SCRIPT_RUNNER_PATH + "/getQPAYReopenedEpicsStatusHistory?";
+    url += "dateFrom=" + dateToYYYYMMDD(prepareFromDate(new Date())) + "&";
+    url += "dateTo=" + dateToYYYYMMDD(new Date()) + "&";
+    url += "subdivision=" + subdivision;
+    window.open(url, "_blank");
+    return false;
+}
+
+function gotoQualityBoard_Mirutov(subdivision) {
+    var url = JIRA_URL + "/" + SCRIPT_RUNNER_PATH + "/getQualityReport?";
+    url += "dateFrom=" + dateToYYYYMMDD(prepareFromDate(new Date())) + "&";
+    url += "dateTo=" + dateToYYYYMMDD(new Date()) + "&";
+    url += "ra=" + subdivision;
+    window.open(url, "_blank");
+    return false;
+}
+
+function gotoChangingDifficultyScoreBoard_Mirutov(subdivision) {
+    var url = JIRA_URL + "/" + SCRIPT_RUNNER_PATH + "/ChangingDifficultyScoreReport?";
+    url += "dateFrom=" + dateToYYYYMMDD(prepareFromDate(new Date())) + "&";
+    url += "dateTo=" + dateToYYYYMMDD(new Date()) + "&";
+    url += "mode=all" + "&";
+    url += "ra=" + subdivision;
     window.open(url, "_blank");
     return false;
 }
@@ -48,37 +107,6 @@ function gotoCollectionUpStream() {
 function gotoOKRReport3() {
 // window.open('https://jira.ftc.ru/rest/scriptrunner/latest/custom/getOKRReport2', '_blank'); return false;
     window.open(JIRA_URL + "/" + SCRIPT_RUNNER_PATH + "/getOKRReport3", "_blank");
-    return false;
-}
-
-function gotoDeptSLA2Collector_Mirutov() {
-// window.open('https://jira.ftc.ru/rest/scriptrunner/latest/custom/getEpicsTimeMetrics2?dateFrom=2023-04-01&dateTo=2023-06-30&ra=cl&size=small&done', '_blank'); return false;
-    var url = JIRA_URL + "/" + SCRIPT_RUNNER_PATH + "/getDeptSLAReport?";
-
-    url += "dateFrom=" + dateToYYYYMMDD(prepareFromDate(new Date())) + "&";
-    url += "dateTo=" + dateToYYYYMMDD(new Date()) + "&";
-    url += "dept=" + "Коллектор" + "&";
-    url += "showEpicName" + "&";
-    url += "printJQL";
-    window.open(url, "_blank");
-    return false;
-}
-
-function gotoEpicsTimeMetrics2Collector_Mirutov() {
-// window.open('https://jira.ftc.ru/rest/scriptrunner/latest/custom/getEpicsTimeMetrics2?dateFrom=2023-04-01&dateTo=2023-06-30&ra=cl&size=small&done', '_blank'); return false;
-    var url = JIRA_URL + "/" + SCRIPT_RUNNER_PATH + "/getFlowTimeMetrics?";
-    url += "dateFrom=" + dateToYYYYMMDD(prepareFromDate(new Date())) + "&";
-    url += "dateTo=" + dateToYYYYMMDD(new Date()) + "&";
-    url += "depts=" + "Коллектор";
-    window.open(url, "_blank");
-    return false;
-}
-
-function gotoStoryPointsBoard(subdivision) {
-// window.open('https://jira.ftc.ru/rest/scriptrunner/latest/custom/getStoryPointsBoard?subdivision=%D0%9A%D0%BE%D0%BB%D0%BB%D0%B5%D0%BA%D1%82%D0%BE%D1%80', '_blank'); return false;
-    var url = JIRA_URL + "/" + SCRIPT_RUNNER_PATH + "/getStoryPointsBoard?";
-    url += "subdivision=" + subdivision;
-    window.open(url, "_blank");
     return false;
 }
 
