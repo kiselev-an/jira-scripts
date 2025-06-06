4eba002c0b0fe62f1456ca5b8003ca68409468b1
Alekey Kiselev <kanic.alex@gmail.com>
2025-06-07 02:43:18 +0700

    DepReport feature: WIP

--- assets/js/load-external-page-js.js
+++ assets/js/load-external-page-js.js
@@ -2,12 +2,12 @@ function loadExternalPage(externalPageURL, onPreparedCallBack) {
     jQuery.get({
         url: externalPageURL,
         success: function(data) {
-            responseHandlerExternalPage(data, onPreparedCallBack);
+            responseHandlerExternalPage(data, externalPageURL, onPreparedCallBack);
         }
     });
 }
 
-function responseHandlerExternalPage(responseData, onPreparedCallBack) {
+function responseHandlerExternalPage(responseData, externalPageURL, onPreparedCallBack) {
     var responseHtml = $.parseHTML(responseData, null);
     $(document).attr("title", $(responseHtml).filter("title").text());
     var externalBodyContent = /<body.*>([\s\S]+)<\/body>/.exec(responseData);
@@ -18,5 +18,32 @@ function responseHandlerExternalPage(responseData, onPreparedCallBack) {
             $(element).remove();
         }
     });
+    $(document).find("img").each(function(index, element) { // replace 'src' attribute for images
+        var externalHost = getHost(externalPageURL);
+        if(null != externalHost) {
+            var newUrl = replaceUrlHost($(element).prop("src"), externalHost);
+            alert($(element).prop("src") + " " + externalHost + " " + newUrl);
+            $(element).prop("src", newUrl);
+        }
+    });
     onPreparedCallBack();
+}
+
+function getHost(url) {
+    try {
+        var urlObj = new URL(url);
+        return urlObj.host;
+    } catch (e) {
+        return null;
+    }
+}
+
+function replaceUrlHost(url, newHost) {
+    try {
+        var urlObj = new URL(url);
+        urlObj.host = newHost;
+        return urlObj.href;
+    } catch (e) {
+        return url;
+    }
 }
