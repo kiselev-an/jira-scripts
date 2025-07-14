786177016b9cc4be281f4361827c272e7dce74d9
Alekey Kiselev <kanic.alex@gmail.com>
2025-07-15 01:36:56 +0700

    DeptReport: WIP - small fixes

--- assets/js/loader-processor.js
+++ assets/js/loader-processor.js
@@ -2,11 +2,13 @@ var activeAjaxRequests = 0;
 
 function showLoader(loaderId) {
     activeAjaxRequests++;
+    console.log("activeAjaxRequests = " + activeAjaxRequests);
     $("#" + loaderId).show(); // Or adjust visibility as needed
 }
 
 function hideLoader(loaderId) {
     activeAjaxRequests--;
+    console.log("activeAjaxRequests = " + activeAjaxRequests);
     if (activeAjaxRequests <= 0) {
         $("#" + loaderId).hide(); // Or adjust visibility as needed
         activeAjaxRequests = 0; // Reset for future use
@@ -15,6 +17,8 @@ function hideLoader(loaderId) {
 
 function processWithLoaderAnimation(process) {
     showLoader("loader_div");
-    process();
+    setTimeout(function() {
+        process();
+    }, 10);
     hideLoader("loader_div");
 }
